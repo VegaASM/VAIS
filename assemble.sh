ba26c0172baea29af1a3842eaf2975f8c6c48a58
VegaASM <zackymcnell@yahoo.com>
2024-12-11 08:59:39 -0600

    fixed Android script

--- assemble.sh
+++ assemble.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Enter directory where vais project directory is at"
+echo "Enter directory where VAIS project directory is at"
 read vaisloc
 
 echo "Enter directory Android NDK root (android-ndk-rXX) directory is at"
 read androidloc
 
 cd $androidloc/android-ndk-r27/toolchains/llvm/prebuilt/linux-x86_64/bin
-./aarch64-linux-android35-clang -o $vaisloc/vais/vais -static $vaisloc/vais/Source/main.S
+./aarch64-linux-android35-clang -o $vaisloc/VAIS/vais -static $vaisloc/VAIS/Source/main.S
 
 echo "Done. Did it work? Who knows."
